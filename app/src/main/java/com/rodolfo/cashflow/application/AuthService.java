4fb3fff7e577cef6679c810c91cfe7e1ec912f70
Rodolfo Collado <rodolfocollado20@gmail.com>
2026-05-03 17:35:13 -0600

    style: some 'TODO' added to AuthService.java

--- app/src/main/java/com/rodolfo/cashflow/application/AuthService.java
+++ app/src/main/java/com/rodolfo/cashflow/application/AuthService.java
@@ -3,6 +3,7 @@ package com.rodolfo.cashflow.application;
 import com.rodolfo.cashflow.domain.contracts.repositories.ICredencialesRepository;
 import com.rodolfo.cashflow.domain.contracts.repositories.IUsuarioRepository;
 import com.rodolfo.cashflow.domain.contracts.services.IAuthService;
+import com.rodolfo.cashflow.domain.exceptions.CredencialesInvalidasException;
 import com.rodolfo.cashflow.domain.models.Credenciales;
 import com.rodolfo.cashflow.domain.models.Usuario;
 
@@ -18,16 +19,28 @@ public class AuthService implements IAuthService {
 
     @Override
     public Usuario iniciarSesion(String username, String password) {
-        Credenciales creds = credRepo.buscarPorUsername(username);
-        if (creds != null && creds.getPassword().equals(password)) {
-            return userRepo.buscarPorId(creds.getUsuarioId());
 
-        }else return null;
+        Credenciales creds = credRepo.buscarPorUsername(username);
+        if (creds == null || !creds.getPassword().equals(password)) { //TODO separar validaciones en un metodo a parte (o clase a parte)
+            throw new CredencialesInvalidasException("Nombre de usuario o contraseña incorrectos.");
+        }
+
+        Usuario user = userRepo.buscarPorId(creds.getUsuarioId());
+
+        if(user == null) {
+            throw new CredencialesInvalidasException("El usuario vinculado a estas credenciales no existe");
+        }
+
+        return user;
     }
 
     @Override
     public void registrar(Usuario usuario, String username, String password) {
-
+        Credenciales creds = new Credenciales();
+        creds.setUsername(username);
+        creds.setPassword(password);
+        creds.setUsuarioId(userRepo.insertar(usuario)); //TODO crear token de inicio de sesión (credenciales) con JWT
+        credRepo.insertar(creds); // TODO crear token con UUID para las transacciones
     }
 
     @Override
