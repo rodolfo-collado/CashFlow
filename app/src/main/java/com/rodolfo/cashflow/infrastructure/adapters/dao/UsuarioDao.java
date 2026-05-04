0332c1d08530658b545f7f23148380eac06e094d
Rodolfo Collado <rodolfocollado20@gmail.com>
2026-05-03 23:09:26 -0600

    style(user-dao): remove unused import

--- app/src/main/java/com/rodolfo/cashflow/infrastructure/adapters/dao/UsuarioDao.java
+++ app/src/main/java/com/rodolfo/cashflow/infrastructure/adapters/dao/UsuarioDao.java
@@ -3,7 +3,7 @@ package com.rodolfo.cashflow.infrastructure.adapters.dao;
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
-import androidx.room.OnConflictStrategy;
+
 import androidx.room.Query;
 import androidx.room.Update;
 
