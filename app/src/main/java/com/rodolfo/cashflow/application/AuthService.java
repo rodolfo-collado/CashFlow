0b67507a58834be9362b2d532ddd6a164963c5f4
Rodolfo Collado <rodolfocollado20@gmail.com>
2026-05-04 21:06:42 -0600

    refactor(auth-service): use validator classes in authenticator service

--- app/src/main/java/com/rodolfo/cashflow/application/AuthService.java
+++ app/src/main/java/com/rodolfo/cashflow/application/AuthService.java
@@ -1,9 +1,14 @@
 package com.rodolfo.cashflow.application;
 
+import com.rodolfo.cashflow.application.segurity.PasswordValidator;
+import com.rodolfo.cashflow.application.segurity.SesionValidator;
+import com.rodolfo.cashflow.application.validators.CredencialesValidator;
+import com.rodolfo.cashflow.application.validators.UsuarioValidator;
 import com.rodolfo.cashflow.domain.contracts.repositories.ICredencialesRepository;
 import com.rodolfo.cashflow.domain.contracts.repositories.IUsuarioRepository;
 import com.rodolfo.cashflow.domain.contracts.services.IAuthService;
 import com.rodolfo.cashflow.domain.exceptions.CredencialesInvalidasException;
+import com.rodolfo.cashflow.domain.exceptions.SesionInvalidaException;
 import com.rodolfo.cashflow.domain.exceptions.usuarioInvalidoException;
 import com.rodolfo.cashflow.domain.models.Credenciales;
 import com.rodolfo.cashflow.domain.models.Usuario;
@@ -16,17 +21,26 @@ public class AuthService implements IAuthService {
     private String tokenSesionActual;
     private final ICredencialesRepository credRepo;
     private final IUsuarioRepository userRepo;
+    private final PasswordValidator passwordValidator;
+    private final SesionValidator sesionValidator;
+    private final CredencialesValidator credencialesValidator;
+    private final UsuarioValidator usuarioValidator;
 
-    public AuthService(ICredencialesRepository credRepo, IUsuarioRepository userRepo) {
+
+    public AuthService(ICredencialesRepository credRepo, IUsuarioRepository userRepo, PasswordValidator passwordValidator, CredencialesValidator credencialesValidator, SesionValidator sesionValidator, UsuarioValidator usuarioValidator) {
         this.credRepo = credRepo;
         this.userRepo = userRepo;
+        this.passwordValidator = passwordValidator;
+        this.credencialesValidator = credencialesValidator;
+        this.sesionValidator = sesionValidator;
+        this.usuarioValidator = usuarioValidator;
     }
 
     @Override
     public void registrar(Usuario usuario, String username, String password, String pin) {
-        validarDatosUsuario(usuario);
-        validarDatosCredenciales(username, password, pin);
-        validarUsernameDisponible(username);
+        usuarioValidator.validarDatosUsuario(usuario); //TODO hacer la validacion del telefono
+        credencialesValidator.validarDatosCredenciales(username, password, pin);
+        credencialesValidator.validarUsernameDisponible(username);
 
         Long usuarioId = userRepo.insertar(usuario);
         Credenciales credenciales = new Credenciales(usuarioId, username, password, pin);
@@ -36,9 +50,9 @@ public class AuthService implements IAuthService {
 
     @Override
     public Usuario iniciarSesion(String username, String password) {
-        validarFormatoLogin(username, password);
-        Credenciales credenciales = obtenerCredencialesValidadas(username, password);
-        this.usuarioActual = obtenerUsuarioValidado(credenciales);
+        credencialesValidator.validarFormatoLogin(username, password);
+        Credenciales credenciales = credencialesValidator.obtenerCredencialesValidadas(username, password);
+        this.usuarioActual = usuarioValidator.obtenerUsuarioValidado(credenciales);
         this.tokenSesionActual = UUID.randomUUID().toString();
         return usuarioActual;
     }
