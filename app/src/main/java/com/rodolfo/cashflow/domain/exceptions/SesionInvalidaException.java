bd497f69d25bc0c027b6f686ba06f3d2ef77c71c
Rodolfo Collado <rodolfocollado20@gmail.com>
2026-05-04 21:04:03 -0600

    feat: add invalid session exception

---
+++ app/src/main/java/com/rodolfo/cashflow/domain/exceptions/SesionInvalidaException.java
@@ -0,0 +1,7 @@
+package com.rodolfo.cashflow.domain.exceptions;
+
+public class SesionInvalidaException extends RuntimeException {
+    public SesionInvalidaException(String message) {
+        super(message);
+    }
+}
