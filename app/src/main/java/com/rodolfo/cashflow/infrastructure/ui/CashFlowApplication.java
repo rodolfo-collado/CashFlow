715d14e2842912df0cdc06ced99ba64afd4e0443
Rodolfo Collado <rodolfocollado20@gmail.com>
2026-05-05 23:08:31 -0600

    feat(app): add application class for hilt

---
+++ app/src/main/java/com/rodolfo/cashflow/infrastructure/ui/CashFlowApplication.java
@@ -0,0 +1,9 @@
+package com.rodolfo.cashflow.infrastructure.ui;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class CashFlowApplication extends Application {
+}
