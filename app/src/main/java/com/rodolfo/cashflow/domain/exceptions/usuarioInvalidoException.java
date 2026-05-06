8c6241b34d825e7323ac8660af4235ca56dcab64
Rodolfo Collado <rodolfocollado20@gmail.com>
2026-05-05 21:59:36 -0600

    feat: add Invalid User Exception

--- app/src/main/java/com/rodolfo/cashflow/domain/exceptions/usuarioInvalidoException.java
+++
@@ -1,7 +0,0 @@
-package com.rodolfo.cashflow.domain.exceptions;
-
-public class usuarioInvalidoException extends RuntimeException {
-    public usuarioInvalidoException(String message) {
-        super(message);
-    }
-}
