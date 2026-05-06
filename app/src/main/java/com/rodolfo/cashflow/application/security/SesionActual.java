ca84255c226173088779e2663e3a6b699c6b8909
Rodolfo Collado <rodolfocollado20@gmail.com>
2026-05-05 22:38:31 -0600

    feat(current-session): add persistence class for current session

---
+++ app/src/main/java/com/rodolfo/cashflow/application/security/SesionActual.java
@@ -0,0 +1,36 @@
+package com.rodolfo.cashflow.application.security;
+
+import com.rodolfo.cashflow.domain.models.Usuario;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Singleton
+@Getter
+@AllArgsConstructor
+public class SesionActual {
+    private Usuario usuarioActual;
+    private String tokenSesionActual;
+
+    @Inject
+    public SesionActual() {
+    }
+
+
+    public void iniciar(Usuario usuario, String token) {
+        this.usuarioActual = usuario;
+        this.tokenSesionActual = token;
+    }
+
+    public void cerrar() {
+        this.usuarioActual = null;
+        this.tokenSesionActual = null;
+    }
+
+    public boolean isActiva() {
+        return usuarioActual != null && tokenSesionActual != null;
+    }
+}
