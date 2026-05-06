ea3e3229f37238fb3752cd2b67774e0c4ee9bf17
Rodolfo Collado <rodolfocollado20@gmail.com>
2026-05-05 23:01:47 -0600

    refactor(credentials-service): rename password change method

--- app/src/main/java/com/rodolfo/cashflow/domain/contracts/services/ICredencialesService.java
+++ app/src/main/java/com/rodolfo/cashflow/domain/contracts/services/ICredencialesService.java
@@ -4,7 +4,7 @@ public interface ICredencialesService {
 
     void cambiarUsername( String password, String username);
 
-    void cambiarContrasena(String oldPassword, String newPassword);
+    void cambiarPassword(String oldPassword, String newPassword);
 
     void cambiarPin(String password, String pin);
 
