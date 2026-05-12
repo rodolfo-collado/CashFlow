920a95b50eb9cb2c64aba871f2c4029aa8a0b254
Rodolfo Collado <rodolfocollado20@gmail.com>
2026-05-11 19:08:10 -0600

    feat(exception): add invalid change exception

---
+++ app/src/main/java/com/rodolfo/cashflow/domain/exceptions/CambioInvalidoException.java
@@ -0,0 +1,7 @@
+package com.rodolfo.cashflow.domain.exceptions;
+
+public class CambioInvalidoException extends RuntimeException {
+    public CambioInvalidoException(String message) {
+        super(message);
+    }
+}
