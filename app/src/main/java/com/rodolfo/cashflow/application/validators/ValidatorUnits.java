d4a740dfff7b938b90a25ff61812c7383b0954fd
Rodolfo Collado <rodolfocollado20@gmail.com>
2026-05-11 19:14:39 -0600

    feat(validator-units): add generic field validation method

--- app/src/main/java/com/rodolfo/cashflow/application/validators/ValidatorUnits.java
+++ app/src/main/java/com/rodolfo/cashflow/application/validators/ValidatorUnits.java
@@ -14,6 +14,17 @@ public class ValidatorUnits {
         }
     }
 
+    public static <T extends RuntimeException> void validarCamposDiferentes(
+            Object campo1,
+            Object campo2,
+            String mensaje,
+            Function<String, T> exceptionFactory
+    ) {
+        if (campo1 == null || campo2 == null || campo1.equals(campo2)) {
+            throw exceptionFactory.apply(mensaje);
+        }
+    }
+
     public static <T extends RuntimeException> void validarNoNulo(
             Object valor,
             String mensaje,
