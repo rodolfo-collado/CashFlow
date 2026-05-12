b928c73323c9c2b90733f1e24e5813943fc35f99
Rodolfo Collado <rodolfocollado20@gmail.com>
2026-05-11 19:17:22 -0600

    fix(user-service): use String for phone number parameter

--- app/src/main/java/com/rodolfo/cashflow/domain/contracts/services/IUsuarioService.java
+++ app/src/main/java/com/rodolfo/cashflow/domain/contracts/services/IUsuarioService.java
@@ -5,7 +5,7 @@ public interface IUsuarioService {
 
     void cambiarCorreo(String password, String correo);
 
-    void cambiarTelefono(String password, Integer telefono);
+    void cambiarTelefono(String password, String telefono);
 
     void cambiarDireccion(String password, String direccion);
 }
