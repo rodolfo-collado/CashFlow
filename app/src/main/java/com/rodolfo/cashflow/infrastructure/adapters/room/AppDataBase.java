ed1316b9f9fdbee7eaee4528a23f25367537549d
Rodolfo Collado <rodolfocollado20@gmail.com>
2026-05-13 15:55:40 -0600

    refactor(database): register Room entities in AppDatabase

--- app/src/main/java/com/rodolfo/cashflow/infrastructure/adapters/room/AppDataBase.java
+++ app/src/main/java/com/rodolfo/cashflow/infrastructure/adapters/room/AppDataBase.java
@@ -4,18 +4,18 @@ import androidx.room.Database;
 import androidx.room.RoomDatabase;
 import androidx.room.TypeConverters;
 
-import com.rodolfo.cashflow.domain.models.Credenciales;
-import com.rodolfo.cashflow.domain.models.Transaccion;
-import com.rodolfo.cashflow.domain.models.Usuario;
 import com.rodolfo.cashflow.infrastructure.adapters.dao.CredencialesDao;
 import com.rodolfo.cashflow.infrastructure.adapters.dao.TransaccionDao;
 import com.rodolfo.cashflow.infrastructure.adapters.dao.UsuarioDao;
 import com.rodolfo.cashflow.infrastructure.adapters.room.converters.BigDecimalConverter;
 import com.rodolfo.cashflow.infrastructure.adapters.room.converters.DateConverter;
+import com.rodolfo.cashflow.infrastructure.adapters.room.entities.CredencialesEntity;
+import com.rodolfo.cashflow.infrastructure.adapters.room.entities.TransaccionEntity;
+import com.rodolfo.cashflow.infrastructure.adapters.room.entities.UsuarioEntity;
 
 
 @TypeConverters({BigDecimalConverter.class, DateConverter.class})
-@Database(entities = {Transaccion.class, Usuario.class, Credenciales.class}, version = 1) // Le indica a Room cuáles son las tablas
+@Database(entities = {TransaccionEntity.class, UsuarioEntity.class, CredencialesEntity.class}, version = 1) // Le indica a Room cuáles son las tablas
 public abstract class AppDataBase extends RoomDatabase {
 
     // Room genera el código automáticamente
